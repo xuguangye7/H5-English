2398117ea4cf723071a6401e8ed43d40dd31accc
xuwwenke <3492517331@qq.com>
2020-05-20 10:27:45 +0800

    写作接口

--- person/薛文科/后台/Route/writ.js
+++ person/薛文科/后台/Route/writ.js
@@ -18,4 +18,36 @@ app.get('/detail',(req,res)=>{
     showdata(res,sql);
 });
 
+var isregister='';
+let inssql = 'INSERT into myanswe(id,answe) VALUES($1,$2)';
+app.post('/add',(req,res)=>{
+    var data = '';
+    req.on('data',(chunk)=>{
+      data += chunk;
+    });
+    req.on('end',()=>{
+        var jsonstr=JSON.parse(data);
+        console.log('jjj',jsonstr)
+        var arr = [];
+        for(let i in jsonstr){
+            arr.push(jsonstr[i]);
+            console.log('js',jsonstr[i])
+        }
+        console.log('arr',arr)
+        isregister=arr[1];
+        console.log('isre',isregister)
+        console.log("Landing successfully");
+        db = { state: 200, message: '提交成功', content: isregister };
+        console.log(jsonstr)
+        pool.query(inssql,arr);
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.json(db);
+    });
+});
+
+
+app.get('/answe',(req,res)=>{
+    let  sql = `SELECT * FROM myanswe WHERE answe='${isregister}'`;
+    showdata(res,sql);
+});
 module.exports=app;
