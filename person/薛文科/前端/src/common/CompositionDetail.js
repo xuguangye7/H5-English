c387380cf51eae33fbd26e599487874d368c1d5d
xuwwenke <3492517331@qq.com>
2020-05-20 10:42:29 +0800

    写作提交答案

--- person/薛文科/前端/src/common/CompositionDetail.js
+++ person/薛文科/前端/src/common/CompositionDetail.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react'
-import { Text, View,DeviceEventEmitter } from 'react-native'
+import { Text,View,DeviceEventEmitter,Animated,Dimensions, TouchableOpacity, StyleSheet, ScrollView, } from 'react-native'
 import { Actions} from 'react-native-router-flux'
 import Header from '../utils/Header'
 import {myFetch} from '../utils/FetchData' 
-import { Button } from '@ant-design/react-native'
+import { Button ,List, TextareaItem, } from '@ant-design/react-native'
 var detail_url='writ/detail';
 var detail_id;
+
+const { width, height } = Dimensions.get("window");
+
 export default class CompositionDetail extends Component {
     constructor(){
         super();
         this.state={
             data:[],
-            title:[]
+            title:[],
+            securetyTipViewY: new Animated.Value(height),
+            val: '',
         }
     }
     componentDidMount(){
@@ -31,10 +36,47 @@ export default class CompositionDetail extends Component {
                 })
             })
     }
+    showView=()=>{
+        Animated.timing(
+            this.state.securetyTipViewY,
+            {
+                toValue: height - 554 - 64,
+                duration: 300,   //动画时长300毫秒
+            }
+        ).start();
+    }
+    hideView=()=>{
+        Animated.timing(
+            this.state.securetyTipViewY,
+            {
+                toValue: height,
+                duration: 300,   //动画时长300毫秒
+            }
+        ).start();
+    }
+    // onChange = (val: any) => {
+    //     console.log(val);
+    //     this.setState({ val });
+    // }
+    userhandle = (text)=>{
+        this.setState({val:text})
+        console.log('tet',text);
+        console.log('sss',this.state.val)
+    }
+    sublimt=()=>{
+        console.log('提交成功')
+        myFetch.post('writ/add',{
+            id:this.state.val.slice(4),
+            val:this.state.val
+        })
+        Actions.answe();
+    }
     render() {
         return (
             <View>
                 <Header name={this.state.title} />
+
+                {/* <TextareaItem editable={true} rows={4} placeholder="在此答题" count={200} /> */}
                 <Text>写作原题</Text>
                 {
                     this.state.data.map((item)=>{
@@ -45,8 +87,26 @@ export default class CompositionDetail extends Component {
                         )
                     })
                 }
-                <Button>开始做题</Button>
+                <Button onPress={this.showView}>开始答题</Button>
+                <Animated.View style={{ position: "absolute",flex:1, top: this.state.securetyTipViewY,width:'100%'}}>
+                    <List renderHeader='答题区'>
+                        <TextareaItem editable={true} rows={7} onChange={this.userhandle} placeholder="在此答题" count={200} />
+                    </List>
+                    <Button onPress={this.sublimt}>提交</Button>
+                    <Button onPress={this.hideView}>取消</Button>
+                </Animated.View>
             </View>
         )
     }
 }
+
+const styles=StyleSheet.create({
+    touchable:{ 
+        flex: 1, 
+        alignItems: 'center', 
+        justifyContent: 'center' ,
+        marginTop:10,
+        width:'100%',
+        backgroundColor:'red'
+    },
+})
